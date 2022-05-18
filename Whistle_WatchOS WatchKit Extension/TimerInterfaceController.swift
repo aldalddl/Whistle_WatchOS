6343308796119e3074f32f1aed084019467460e2
Minji Kang <aldalddl2007@gmail.com>
2022-05-18 19:26:28 +0900

    countdown
    Countdown using Timer() and
    Display using WKInterfaceTimer()

--- Whistle_WatchOS WatchKit Extension/TimerInterfaceController.swift
+++ Whistle_WatchOS WatchKit Extension/TimerInterfaceController.swift
@@ -11,6 +11,8 @@ import Foundation
 
 class TimerInterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var WKTimer: WKInterfaceTimer!
+    
     @IBOutlet weak var minutePicker: WKInterfacePicker!
     @IBOutlet weak var secondPicker: WKInterfacePicker!
     
@@ -19,13 +21,20 @@ class TimerInterfaceController: WKInterfaceController {
     
     @IBOutlet weak var listPickerGroup: WKInterfaceGroup!
     
-    var ounces = 30
-    
+    var minuteOunces = 1
+    var secondOunces = 1
+
     enum WatchStatus {
         case start
         case stop
     }
     var watchStatus: WatchStatus = .start
+    
+    var timer = Timer()
+    var minutesPassed = 0
+    var secondsPassed = 0
+    var minuteTotalTime = 0
+    var secondTotalTime = 0
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -45,35 +54,37 @@ class TimerInterfaceController: WKInterfaceController {
     }
 
     @IBAction func minutePickerChanged(_ value: Int) {
-        ounces = value + 1
+        minuteOunces = value + 1
+        print("minute value: \(value)")
     }
     
     @IBAction func secondPickerChanged(_ value: Int) {
-        ounces = value + 1
+        secondOunces = value + 1
+        print("second value: \(value)")
     }
     
     func minutePickerTimer() {
         var minuteItems: [WKPickerItem] = []
         
-        for i in 1...32 {
+        for i in 0...59 {
             let item_minute = WKPickerItem()
             item_minute.title = String(i)
             minuteItems.append(item_minute)
         }
         minutePicker.setItems(minuteItems)
-        minutePicker.setSelectedItemIndex(ounces - 1)
+        minutePicker.setSelectedItemIndex(minuteOunces - 1)
     }
     
     func secondPickerTimer() {
         var secondItems: [WKPickerItem] = []
         
-        for j in 1...32 {
+        for j in 0...59 {
             let item_second = WKPickerItem()
             item_second.title = String(j)
             secondItems.append(item_second)
         }
         secondPicker.setItems(secondItems)
-        secondPicker.setSelectedItemIndex(ounces - 1)
+        secondPicker.setSelectedItemIndex(secondOunces - 1)
     }
     
     @IBAction func start() {
@@ -84,6 +95,23 @@ class TimerInterfaceController: WKInterfaceController {
                 startButton.setBackgroundColor(UIColor.orange)
 //                sequencePickerGroup.setHidden(true)
 //                listPickerGroup.setHidden(true)
+            
+                timer.invalidate()
+                minuteTotalTime = minuteOunces - 1
+                secondTotalTime = secondOunces - 1
+                
+                secondsPassed = 0
+                minutesPassed = 0
+            
+                let minute:TimeInterval = Double(minuteTotalTime) * 60.0
+                let second:TimeInterval = Double(secondTotalTime)
+                let totalTime: TimeInterval = minute + second
+                let date = Date(timeIntervalSinceNow: totalTime)
+
+                timer = Timer.scheduledTimer(timeInterval: totalTime, target: self, selector: #selector(timerDone), userInfo: nil, repeats: true)
+                WKTimer.setDate(date)
+                WKTimer.start()
+            
             case .stop:
                 self.watchStatus = .start
                 startButton.setTitle("Start")
@@ -91,4 +119,11 @@ class TimerInterfaceController: WKInterfaceController {
         }
     }
     
+    @objc func timerDone(){
+        //timer done counting down
+        timer.invalidate()
+        print("End.")
+    }
 }
+
+
